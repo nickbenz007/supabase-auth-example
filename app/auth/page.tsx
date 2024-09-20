f8e65419b22160a746f6679a801518d31ed73ee5
nickbenz007 <nickbenz007@gmail.com>
2024-09-20 18:21:43 +0500

    fixing the redirection

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -43,7 +43,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/https://supabase-auth-example-seven.vercel.app`,
+        redirectTo: `${location.origin}/`,
       },
     });
     if (error) {
