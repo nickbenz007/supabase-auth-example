0ffc37b31f3fe4fb7cbeb420272a600565129d60
nickbenz007 <nickbenz007@gmail.com>
2024-09-21 15:30:21 +0500

    adding production .env

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -43,7 +43,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: location.origin + "/auth/v1/callback",
+        redirectTo: location.origin + "/auth/callback",
       },
     });
     if (error) {
