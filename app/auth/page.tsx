47395836d7b55e18442e73e5984b7d261daa433e
nickbenz007 <nickbenz007@gmail.com>
2024-09-21 14:44:31 +0500

    remove required from input

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -43,7 +43,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin + "/auth/callback",
+        redirectTo: location.origin + "/callback",
       },
     });
     if (error) {
