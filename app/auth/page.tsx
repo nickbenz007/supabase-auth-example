eeacca71ec8296487a088c370eca5dc698032c20
nickbenz007 <nickbenz007@gmail.com>
2024-09-21 15:19:54 +0500

    replacing the redirect ulr

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -43,7 +43,7 @@ const Auth = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: location.origin + "/callback",
+        redirectTo: location.origin + "/auth/v1/callback",
       },
     });
     if (error) {
