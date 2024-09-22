11ef318ad679535a93ebd5b64bdd7d9e4540deb0
nickbenz007 <nickbenz007@gmail.com>
2024-09-22 18:53:53 +0500

    fixing

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,13 +15,14 @@ export async function GET(request: Request) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
 
-      const redirectUrl = isLocalEnv
-        ? `${origin}${next}`
-        : forwardedHost
-        ? `https://${forwardedHost}${next}`
-        : `${origin}${next}`;
-
-      return NextResponse.redirect(redirectUrl);
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
