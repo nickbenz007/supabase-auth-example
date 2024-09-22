12785395849ca9396d028b7e31885335e9a813f0
nickbenz007 <nickbenz007@gmail.com>
2024-09-23 00:52:40 +0500

    fixied redirecting finally

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -41,10 +41,22 @@ const Auth = () => {
   };
 
   const handleSignInWithOAuth = async (provider: "google" | "github") => {
+    const getUrl = () => {
+      let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/"; // Default to localhost in development.
+
+      url = url.startsWith("http") ? url : `https://${url}`;
+
+      url = url.endsWith("/") ? url : `${url}/`;
+
+      return url;
+    };
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: location.origin + "/auth/callback",
+        redirectTo: getUrl(),
       },
     });
 
